7226422287655d2860b3b71d529a000fb9c6d3f7
Nisarg Kanojia <nisarg@agingo.com>
2023-12-21 22:18:38 +0000

    modifing graph, find the x above 33% from dataset1

--- 22085391.py
+++ 22085391.py
@@ -18,13 +18,12 @@ def read_data():
 ###### Main Function ######
 
 data = read_data()
-print(np.count_nonzero(data))
 
 ohist, oedge = np.histogram(data, bins=32)
 
 
 # calculate bin centre locations and bin widths
-xdst=0.5*(oedge[1:]+oedge[:-1])
+xdst= (oedge[1:]+oedge[:-1])/2
 wdst=oedge[1:]-oedge[:-1]
 
 # normalise the distribution
@@ -50,5 +49,26 @@ plt.plot([xmean,xmean],[0.0,max(ydst)], c='red')
 text = ''' Mean value: {}'''.format(xmean.astype(int))
 plt.text(x=xmean, y=max(ydst), s=text, fontsize=12, c='red')
 
+
+#The value of X should be such that 33% of people have a salary above X.
+indx=np.argmin(np.abs(cdst-0.67))
+xlow=oedge[indx]
+
+plt.bar(xdst[indx:],ydst[indx:], width=0.9*wdst[indx:], color='green')
+
+#find the fraction of claims £5k+
+hdst=ydst*(xdst >= 56003)
+#plt.bar(xdst, hdst, width=0.9*wdst, color='orange')
+
+
+
+plt.plot([xlow,xlow],[0.0,max(ydst)], c='orange')
+text = ''' 33% of people have a \nsalary above {}'''.format(xlow.astype(int))
+plt.text(x=xlow+xlow*0.10, y=max(ydst)-0.012, s=text, fontsize=12, c='orange')
+
+#plt.figure(1)
+#plt.hist(data, bins=32, edgecolor='w',density=True)
+#plt.xlabel('Claim value, £', fontsize=15)
+#plt.ylabel('Probability', fontsize=15)
 
 plt.show()
