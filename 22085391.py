eb68c4f97c65af0cc2deb0618d684cf534bb6a02
Nisarg Kanojia <nisarg@agingo.com>
2024-01-10 02:29:29 +0000

    pep8

--- 22085391.py
+++ 22085391.py
@@ -8,6 +8,7 @@ Created on Sun Dec 17 14:07:41 2023
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def read_data():
     """
     Return the read csv file return salar.
@@ -18,9 +19,10 @@ def read_data():
         return ther numby array list.
 
     """
-    #read csv file
+    # read csv file
     data = np.genfromtxt('data1-1.csv', delimiter=',')
     return data
+
 
 def calculate_bin_size(data):
     """
@@ -40,67 +42,89 @@ def calculate_bin_size(data):
     # Calculate bin size using Freedman-Diaconis rule
     iqr = np.percentile(data, 75) - np.percentile(data, 25)
     bin_width = int(2 * iqr / (len(data) ** (1/3)))
-    bin_size = np.ceil((max(data)- min(data))/bin_width).astype(int)
-    print("bin_size",bin_size)
+    bin_size = np.ceil((max(data) - min(data))/bin_width).astype(int)
+    print("bin_size", bin_size)
     return bin_size
-    
-def calculate_pdf(data):
+
+
+def calculate_pdf(pdf_data):
     """
-    Calculate the psd mean
+    Calculate the psd mean.
 
     Parameters
     ----------
-    data : numpy array
+    pdf_data : numpy array
         data for which pdf to be calculated.
 
     Returns
     -------
     xdst : int
         calculate bin centre locations and bin widths.
-    ydst : TYPE
+    ydst : int
         ydist is a discrete PDF.
-    wdst : TYPE
+    wdst : int
         width of bins.
-    cdst : TYPE
+    cdst : int
         cumulative distribution.
-    oedge : TYPE
+    oedge : int
         bin edges.
-    xmean : TYPE
+    xmean : int
         mean value.
 
     """
-    #get the bin size
-    bin_size = calculate_bin_size(data)
+    # get the bin size
+    bin_size = calculate_bin_size(pdf_data)
     ohist, oedge = np.histogram(data, bins=bin_size)
 
     # calculate bin centre locations and bin widths
-    xdst= (oedge[1:]+oedge[:-1])/2
-    wdst=oedge[1:]-oedge[:-1]
+    xdst = (oedge[1:]+oedge[:-1])/2
+    wdst = oedge[1:]-oedge[:-1]
 
     # normalise the distribution
-    #ydist is a discrete PDF
+    # ydist is a discrete PDF
     ydst = ohist/np.sum(ohist)
 
-    #cumulative distribution
-    cdst=np.cumsum(ydst)
-    
-    #Mean value
-    xmean=np.sum(xdst*ydst).round(2)
-    print("Mean : " , data.mean())
-    print("PDF Mean(W) : " , xmean)
-    
+    # cumulative distribution
+    cdst = np.cumsum(ydst)
+
+    # Mean value
+    xmean = np.sum(xdst*ydst).round(2)
+    print("Mean : ", data.mean())
+    print("PDF Mean(W) : ", xmean)
+
     return xdst, ydst, wdst, cdst, oedge, xmean
 
-def calculate_salary_below_33(cdst,oedge):
-    #The value of X should be such that 33% of people have a salary above X.
-    indx=np.argmin(np.abs(cdst-0.67))
-    xlow=oedge[indx].round(2)
-    print("X:",xlow)
-    return xlow,indx
 
-def plot_graph(data):
+def calculate_salary_below_33(cdst, oedge):
     """
-    plot the graph and display values on ot
+    calculate the value of X should be such that 33% of peo\
+ple have a salary above X
+
+    Parameters
+    ----------
+    cdst : TYPE
+        DESCRIPTION.
+    oedge : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    xlow : float
+        salary.
+    indx : inr
+        position of bar.
+
+    """
+    # The value of X should be such that 33% of people have a salary above X.
+    indx = np.argmin(np.abs(cdst-0.67))
+    xlow = oedge[indx].round(2)
+    print("X:", xlow)
+    return xlow, indx
+
+
+def plot_graph(plot_data):
+    """
+    plot the graph and display values on it.
 
     Parameters
     ----------
@@ -112,10 +136,9 @@ def plot_graph(data):
     None.
 
     """
-    
-    xdst, ydst, wdst, cdst, oedge, xmean = calculate_pdf(data)
-    
-    plt.figure(0,dpi=300)
+    xdst, ydst, wdst, cdst, oedge, xmean = calculate_pdf(plot_data)
+
+    plt.figure(0, dpi=300)
 
     # Plot the PDF
     plt.bar(xdst, ydst, width=0.8*wdst)
@@ -124,28 +147,28 @@ def plot_graph(data):
     plt.ylabel('Probability', fontsize=15)
     plt.title("Salaries distribution of European country")
 
-    #and plot it
+    # and plot it
     text = ''' Mean value(W): {}'''.format(xmean.astype(float))
-    plt.plot([xmean,xmean],[0.0,max(ydst)], c='red', label=text)
+    plt.plot([xmean, xmean], [0.0, max(ydst)], c='red', label=text)
 
-    xlow, indx = calculate_salary_below_33(cdst,oedge)
-    
-    plt.bar(xdst[indx:],ydst[indx:], width=0.9*wdst[indx:], color='green')
+    xlow, indx = calculate_salary_below_33(cdst, oedge)
+
+    plt.bar(xdst[indx:], ydst[indx:], width=0.9*wdst[indx:], color='green')
 
     text =  \
-    ''' 33% of people have a \nsalary above(X) {}'''.format(xlow.astype(float))
-    plt.plot([xlow,xlow],[0.0,max(ydst)], c='orange', label=text)
+        ''' 33% of people have a \nsalary above(X) {}'''.format(
+            xlow.astype(float))
+    plt.plot([xlow, xlow], [0.0, max(ydst)], c='orange', label=text)
 
     plt.legend()
-    plt.savefig("22085391.png",dpi=300)
+    plt.savefig("22085391.png", dpi=300)
     plt.show()
-    
-    
+
+
 ###### Main Function ######
 
-#read the csv data
+# read the csv data
 data = read_data()
 
-#calculate the pdf and plot the graph
+# calculate the pdf and plot the graph
 plot_graph(data)
-
